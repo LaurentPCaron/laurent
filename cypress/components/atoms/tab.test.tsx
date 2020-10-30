dee2514684512480d1d4b49a7102babad8ba0a0f
Laurent-Philippe Caron <laurentpcaron@gmail.com>
2020-10-30 09:50:21 -0400

    MAJ

--- cypress/components/atoms/tab.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { mount } from 'cypress-react-unit-test';
-
-describe('Molecul Tab', ()=>{
-    it('should exist',());
-    it('can be selected');
-
-})
--- src/components/atoms/tab.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Tab =()=>{
-
-}
-
-export default Tab
